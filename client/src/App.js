4dc4e56edbcbb8abaf783809ba3ed00ccf455ed5
gavinward117 <gavinward117@gmail.com>
2021-10-20 18:53:15 -0400

    Added new webapp UI

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,102 @@
-import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import { NavLink } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
 
-import Home from './components/Home';
-import Page1 from './components/Page1';
-import Page2 from './components/Page2';
-import Error from './components/Error';
-import Navigation from './components/Navigation';
- 
-class App extends Component {
-  render() {
-    return (      
-       <BrowserRouter>
-        <div>
-        
-          <Navigation />
-            <Switch>
-             <Route path="/" component={Home} exact/>
-             <Route path="/CenterArt" component={Page1}/>
-             <Route path="/CropArt" component={Page2}/>
-            <Route component={Error}/>
-           </Switch>
-        </div> 
-      </BrowserRouter>
-    );
-  }
+function App() {
+  return (
+    <div className="App">
+      <header class="App-header">
+        <div class="col-12">
+          HDRTZ Configuration Menu
+        </div>
+      </header>
+
+
+      <body class="App-body">
+
+        <div class="row">
+          <div class="col-3">
+          </div>
+          <div class="col-6">
+
+            <div class="row">
+              <div class="col-12">
+                Move Camera:
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-3">
+                <button type="button" class="btn btn-color col-10">Up</button>
+              </div>
+              <div class="col-3">
+                <button type="button" class="btn btn-color col-10">Down</button>
+              </div>
+              <div class="col-3">
+                <button type="button" class="btn btn-color col-10">Left</button>
+              </div>
+              <div class="col-3">
+                <button type="button" class="btn btn-color col-10">Right</button>
+              </div>
+            </div>
+            <div class="row">
+              &nbsp;
+            </div>
+            <div class="row">
+              <div class="col-12">
+                Zoom:
+              </div>
+            </div>
+
+            <div class="row">
+              <div class="col-1">1x</div>
+              <div class="col-10">
+                <input type="range" class="form-range" min="0" max="100"></input>
+              </div>
+              <div class="col-1">4x</div>
+            </div>
+            <div class="row">
+              &nbsp;
+            </div>
+            <div class="row">
+              <div class="col-3">
+                Mask:
+              </div>
+              <div class="col-3">
+                <label class="switch switch-flat switch-format">
+                  <input class="switch-input" type="checkbox" />
+                  <span class="switch-label" data-on="On" data-off="Off"></span>
+                  <span class="switch-handle"></span>
+                </label>
+              </div>
+              <div class="col-3">
+                Crosshair:
+              </div>
+              <div class="col-3">
+                <label class="switch switch-flat switch-format">
+                  <input class="switch-input" type="checkbox" />
+                  <span class="switch-label" data-on="On" data-off="Off"></span>
+                  <span class="switch-handle"></span>
+                </label>
+              </div>
+              <div class="row">
+                &nbsp;
+              </div>
+              <div class="row">
+                <div class="col-3">
+                </div>
+                <div class="col-6">
+                  <button type="button" class="btn btn-color col-10">Calibrate Crank When Idle</button>
+                </div>
+                <div class="col-3">
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-3">
+          </div>
+        </div>
+      </body>
+    </div>
+  );
 }
- 
+
 export default App;
